08db3fca9178ac11fd27954af6fafbb0000626d1
赤霄 <237768986@qq.com>
2018-12-25 12:15:07 +0800

    [+] vue对接websocket

---
+++ demos/vue-websocket/websocket.vue
@@ -0,0 +1,34 @@
+<template></template>
+
+<script>
+export default {
+  data() {
+    return {
+      ws: null
+    };
+  },
+  created() {
+    if ("window" in window) {
+      this.initSocket();
+    } else {
+      alert("your browser not support socket!");
+    }
+  },
+  methods: {
+    initSocket() {
+      let _this = this;
+      const wsName = "ws://********/api/webSocket/";
+      this.ws = new WebSocket(wsName);
+      this.ws.onopen = function(evt) {};
+      this.ws.onmessage = function(evt) {
+        // 获取服务端的推送的消息
+        console.log(evt);
+        _this.ws.send("我收到消息了");
+      };
+    }
+  }
+};
+</script>
+
+<style>
+</style>
