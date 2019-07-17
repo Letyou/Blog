45f1b9442de107e3e8d5fd06a69fcd3fff28dc5e
liuxiongjian <chixiao@webuy.ai>
2019-07-17 17:27:27 +0800

    refactor: 优化项目结构

---
+++ Components/README.md
@@ -0,0 +1,19 @@
+# 组件库
+
+- [wechat-app](https://github.com/Letyou/Blog/blob/master/Components/wechat-app)
+
+- [确认框](https://github.com/Letyou/Blog/blob/master/Components/confirm/confirm.vue)
+
+-[加载提示](https://github.com/Letyou/Blog/blob/master/Components/loading/loading.vue)
+
+-[步进器](https://github.com/Letyou/Blog/blob/master/Components/stepper/stepper.vue)
+
+## 勘误及提问
+
+如果有疑问或者发现错误，可以在相应的 issues 进行提问或勘误。
+
+如果喜欢或者有所启发，欢迎 star，对作者也是一种鼓励。
+
+## License
+
+所有文章采用[知识共享署名-非商业性使用-相同方式共享 3.0 中国大陆许可协议](http://creativecommons.org/licenses/by-nc-sa/3.0/cn/)进行许可。
--- study/HttpRequest/http.js
+++ study/HttpRequest/http.js
@@ -1,10 +1,9 @@
 /*
-* Common configuration
-* BASE_URL = 服务器地址
-*/
-const BASE_URL = 'https://baidu.com'
-let version = '2.0.0';
-
+ * Common configuration
+ * BASE_URL = 服务器地址
+ */
+const BASE_URL = "https://baidu.com";
+let version = "2.0.0";
 
 /*
 * Request header configuration
@@ -13,19 +12,19 @@ let version = '2.0.0';
   @params:{Accept}  请求头并非实体头
 */
 const HEADER_OPTIONS = {
-  'X-App-Version': version,
-  'x-platform': 'Alipay',
-  'Accept': 'application/json',
-}
+  "X-App-Version": version,
+  "x-platform": "Alipay",
+  Accept: "application/json"
+};
 // Network request
-const http = ({ url = '', params = {}, ...other }) => {
+const http = ({ url = "", params = {}, ...other }) => {
   // The polling interface is hidden separately
-  if (url === '/miniprogram/sign/checkBind') {
+  if (url === "xxxx") {
     my.hideLoading();
   } else {
     my.showLoading({
-      content: '载入中...'
-    })
+      content: "载入中..."
+    });
   }
   return new Promise((resolve, reject) => {
     my.request({
@@ -34,66 +33,66 @@ const http = ({ url = '', params = {}, ...other }) => {
       headers: HEADER_OPTIONS,
       ...other,
       timeout: 5000,
-      complete: (res) => {
+      complete: res => {
         my.hideLoading();
         if (res.data.return) {
           resolve(res.data);
         } else {
-          reject(res.data)
+          reject(res.data);
           switch (res.status) {
             case 404:
               my.showToast({
-                content: '网络请求不存在',
-                type: 'fail',
-                duration: 2000,
+                content: "网络请求不存在",
+                type: "fail",
+                duration: 2000
               });
               break;
             case 502:
               my.showToast({
-                content: '服务器错误',
-                type: 'fail',
-                duration: 2000,
+                content: "服务器错误",
+                type: "fail",
+                duration: 2000
               });
               break;
             case 504:
               my.showToast({
-                content: '请求服务器超时',
-                type: 'fail',
-                duration: 2000,
+                content: "请求服务器超时",
+                type: "fail",
+                duration: 2000
               });
               break;
           }
           // 接口返回通用异常处理
           if (res.data.ecode === -9999) {
             my.alert({
-              title: '温馨提示',
+              title: "温馨提示",
               content: res.data.message
             });
           }
         }
       }
-    })
-  })
-}
+    });
+  });
+};
 const getUrl = url => {
-  if (url.indexOf('://') == -1) {
-    url = BASE_URL + url
+  if (url.indexOf("://") == -1) {
+    url = BASE_URL + url;
   }
-  return url
-}
+  return url;
+};
 module.exports = {
   BASE_URL,
   get(url, params = {}) {
     return http({
       url,
       params
-    })
+    });
   },
   post(url, params = {}) {
     return http({
       url,
       params,
-      method: 'post'
-    })
-  },
-}
+      method: "post"
+    });
+  }
+};
